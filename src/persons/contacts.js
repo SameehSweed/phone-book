bf6ee7cab5dcdd66a1f912320ce68814a7c4ce8a
sameeh sweed <88625429+SameehSweed@users.noreply.github.com>
2021-09-09 12:41:09 +0300

    Add files via upload

---
+++ src/persons/contacts.js
@@ -0,0 +1,53 @@
+
+
+     const contacts =[
+        {
+          name: 'median',
+          phone:"123456879",
+          address:"haifa",
+          email:"median@gmail.com",
+          imgSrc: 'https://image.shutterstock.com/image-photo/head-shot-young-attractive-businessman-260nw-1854697390.jpg',
+          text: 'I am winner',
+        },
+        {
+            name: 'sameeh',
+            phone:"123456879",
+            address:"haifa",
+            email:"median@gmail.com",
+            imgSrc: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+            text: 'I am winner',
+        },
+        {
+            name: 'moha',
+            phone:"123456879",
+            address:"haifa",
+            email:"median@gmail.com",
+            imgSrc: 'https://img.freepik.com/free-photo/playful-hot-african-american-with-afro-hairstyle-pulling-hands-towards-make-selfie-winking-joyfully-smiling-broadly-making-new-profile-pic-social-network_176420-23120.jpg?size=626&ext=jpg&ga=GA1.2.1377421482.1624924800',
+            text: 'I am winner',
+        },
+        {
+            name: 'tres',
+            phone:"123456879",
+            address:"haifa",
+            email:"median@gmail.com",
+            imgSrc: 'https://makehimyours.com.au/wp-content/uploads/2016/11/Depositphotos_9830876_l-2015Optimised.jpg',
+            text: 'I am winner',
+        },
+        {
+          name: 'tania',
+          phone:"123456879",
+          address:"haifa",
+          email:"median@gmail.com",
+          imgSrc: 'https://media.gettyimages.com/photos/portrait-of-a-girl-picture-id938709362?s=612x612',
+          text: 'I am winner',
+      },
+       
+      ]
+
+
+
+
+
+
+
+export default contacts
